e6d68cb87db6bdd128a057a3b88af8b99872dda6
karbi22 <201801280b@utea.edu.pe>
2021-11-26 21:32:45 -0500

    subiendo los primeros archivos de login

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </head>
 <body>
    <form action="validar.php" method="post">
-   <h1>Sistema de login</h1>
+   <h1>SISTEMA DE LOGIN</h1>
    <p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario"></p>
    <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
    <input type="submit" value="Ingresar">
